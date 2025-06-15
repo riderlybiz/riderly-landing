2ab425c1d4c12993d0008e7304b392c5ad365dcf
유민호 <ymh0951@naver.com>
2025-06-15 21:50:27 +0900

    사전 신청

--- src/app/components/ApplicationForm.tsx
+++ src/app/components/ApplicationForm.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
-import { ApplicationFormWrap } from '../styles/ApplicationFormStyled'
+import { ApplicationDescription, ApplicationFormWrap, ApplicationTitle, ApplicationTitleWrap } from '../styles/ApplicationFormStyled'
 
 export const ApplicationForm = () => {
   return (
-    <ApplicationFormWrap>ApplicationForm</ApplicationFormWrap>
+    <ApplicationFormWrap>
+      <ApplicationTitleWrap>
+        <ApplicationTitle>사전 신청</ApplicationTitle>
+        <ApplicationDescription>
+          Riderly 출시 소식을 가장 먼저 받아보세요!<br />
+          사전 신청자에게는 특별한 혜택을 준비했습니다.
+        </ApplicationDescription>
+      </ApplicationTitleWrap>
+    </ApplicationFormWrap>
   )
 }
--- src/app/styles/ApplicationFormStyled.tsx
+++ src/app/styles/ApplicationFormStyled.tsx
@@ -2,4 +2,26 @@ import styled from "styled-components";
 
 export const ApplicationFormWrap = styled.section`
     padding: 50px 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const ApplicationTitleWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+`
+
+export const ApplicationTitle = styled.h2`
+    font-weight: 700;
+    font-size: 32px;
+`
+
+export const ApplicationDescription = styled.p`
+    text-align: center;
+    font-size: 24px;
+    line-height: 28px;
+    color: #6B747F;
 `
