db6947549868d67c7a3b98542aee90e239b14167
유민호 <ymh0951@naver.com>
2025-06-15 18:45:46 +0900

    Production

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/riderly-landing',
+  basePath: isProduction ? '/riderly-landing' : '',
   compiler: {
     styledComponents: true,
   },
