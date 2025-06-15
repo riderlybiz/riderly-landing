1b71297f871fe1753a66db4c863e29d6d261756b
유경호 <ykh6242@gmail.com>
2025-06-16 02:11:54 +0900

    MainSection에 존재하는 사전신청 버튼 스크롤 함수 추가

--- src/app/components/MainSection.tsx
+++ src/app/components/MainSection.tsx
@@ -3,26 +3,39 @@
 import { MainSectionBtn, MainSectionBtnWrap, MainSectionContent, MainSectionLogoImg, MainSectionSubText, MainSectionText, MainSectionTextWrap, MainSectionWrap } from '@/app/styles/MainSectionStyled';
 
 export const MainSection = () => {
-  return (
-    <MainSectionWrap id='main-sec'>
-        <MainSectionContent>
-            <MainSectionLogoImg src="./logo.png" alt="Riderly" fill />
-            <MainSectionTextWrap>
-                <MainSectionText>
-                    혼자 달리기 외로운<br />
-                    라이더들을 위한<br />
-                    벙개 모임 플랫폼
-                </MainSectionText>
-                <MainSectionSubText>
-                    동호회 활동 부담 없이 쉽게 라이딩 동행자를 찾을 수 있는<br />
-                    새로운 라이더 커뮤니티 서비스
-                </MainSectionSubText>
-            </MainSectionTextWrap>
-            <MainSectionBtnWrap>
-                <MainSectionBtn>사전신청하고 출시알림 받기</MainSectionBtn>
-                <MainSectionBtn>서비스 둘러보기</MainSectionBtn>
-            </MainSectionBtnWrap>
-        </MainSectionContent>
-    </MainSectionWrap>
-  )
+
+    const handleClickAutoScroll = (id: string) => {
+        const element = document.querySelector<HTMLElement>(`#${id}`);
+
+        if (element) {
+            window.scrollTo({
+                top: element.offsetTop - 80, // 예: 헤더 높이만큼 여유
+                behavior: 'smooth',
+            });
+        }
+    };
+
+    return (
+        <MainSectionWrap id='main-sec'>
+            <MainSectionContent>
+                <MainSectionLogoImg src="./logo.png" alt="Riderly" fill />
+                <MainSectionTextWrap>
+                    <MainSectionText>
+                        혼자 달리기 외로운<br />
+                        라이더들을 위한<br />
+                        벙개 모임 플랫폼
+                    </MainSectionText>
+                    <MainSectionSubText>
+                        동호회 활동 부담 없이 쉽게 라이딩 동행자를 찾을 수 있는<br />
+                        새로운 라이더 커뮤니티 서비스
+                    </MainSectionSubText>
+                </MainSectionTextWrap>
+                <MainSectionBtnWrap>
+                    <MainSectionBtn
+                        onClick={() => handleClickAutoScroll('application-form')}
+                    >사전신청하고 출시알림 받기</MainSectionBtn>
+                </MainSectionBtnWrap>
+            </MainSectionContent>
+        </MainSectionWrap>
+    )
 }
