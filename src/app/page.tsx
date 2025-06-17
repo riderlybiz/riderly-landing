21a75212fb311d9c2430b3eae80bce02fa6d94a8
유경호 <ykh6242@gmail.com>
2025-06-17 11:17:30 +0900

    UTM 파라미터 수집을 위한 우회 구조 생성
    - 진입 채널 별로 UTM 파리미터를 적용한 URL로 리다이렉트 되도록 구조를 짬.
    - 메인 페이지로 랜딩된 이후로는 UTM 파라미터를 제거한 깔끔한 URL로 되돌림.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+'use client';
+
 import { Main } from "./components/Main";
+import { useCleanUrl } from './util/useCleanUrl';
 
 export default function Home() {
+  // useCleanUrl 훅을 사용하여 URL 정리
+  // 이 훅은 URL에서 불필요한 부분을 제거하고, 깔끔한 URL을 유지합니다.
+  useCleanUrl();
+  
   return (
     <Main />
   );
