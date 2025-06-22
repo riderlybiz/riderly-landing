2f162be8d887a61276c83aac411918130272ec69
유경호 <ykh6242@gmail.com>
2025-06-23 06:05:57 +0900

    롤백

--- src/lib/firebase/index.ts
+++ src/lib/firebase/index.ts
@@ -1,20 +1,14 @@
-import { decrypt } from '@/utils/crypto';
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-if (!process.env.ENCRYPTION_KEY) {
-  throw new Error('ENCRYPTION_KEY 환경변수가 정의되지 않았습니다.');
-}
-
-// riderly firebase
 const firebaseConfig = {
-  apiKey: decrypt(process.env.NEXT_PUBLIC_FIREBASE_API_KEY!, process.env.ENCRYPTION_KEY!),
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+    apiKey: "AIzaSyBthjX1-i8wdnBhf_uufyChe0QC2ukqfns",
+    authDomain: "riderly-2cda0.firebaseapp.com",
+    projectId: "riderly-2cda0",
+    storageBucket: "riderly-2cda0.firebasestorage.app",
+    messagingSenderId: "874358645048",
+    appId: "1:874358645048:web:20bac4af2d462c158461cd"
+}
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
